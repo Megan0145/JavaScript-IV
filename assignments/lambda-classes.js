65be854366fcef1fb0b40a4f7a4ce04e7f83071e
Megan <mennis0145@gmail.com>
2019-08-22 15:47:05 +0100

    Tidied up answers for improved readibility

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,12 +1,6 @@
 // CODE here for your Lambda Classes
 
-// #### Person
-
-// * First we need a Person class. This will be our `base-class`
-// * Person receives `name` `age` `location` all as props
-// * Person receives `speak` as a method.
-// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
-
+// PERSON
 class Person {
     constructor(name, age, location){
         this.name = name;
@@ -22,18 +16,8 @@ let newPerson1 = new Person('Meg', 21, 'Donabate');
 let newPerson2 = new Person('Lola', 18, 'Howth');
 let newPerson3 = new Person('Jack', 27, 'Sutton');
 
-// #### Instructor
 
-// * Now that we have a Person as our base class, we'll build our Instructor class.
-// * Instructor uses the same attributes that have been set up by Person
-// * Instructor has the following unique props:
-//   * `specialty` what the Instructor is good at i.e. 'redux'
-//   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-//   * `catchPhrase` i.e. `Don't forget the homies`
-// * Instructor has the following methods:
-//   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-//   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
-
+//INSTRUCTOR
 class Instructor extends Person{
     constructor(name, age, location, speciality, favLanguage, catchPhrase){
         super(name, age, location);
@@ -60,21 +44,8 @@ let newInstructor1 = new Instructor('Johnny', 40, 'Dun Laoghaire', 'Frontend', '
 let newInstructor2 = new Instructor('Catherine', 51, 'Rathgar', 'iOS Development', 'Ruby on Rails', 'Swinging For the Fences');
 let newInstructor3 = new Instructor('Stefan', 38, 'Lucan', 'Computer Architecture', 'JS', 'Keep On Truckin');
 
-// #### Student
 
-// * Now we need some students!
-// * Student uses the same attributes that have been set up by Person
-// * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-// * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
-
-
-
+//STUDENT
 class Student extends Person{
     constructor(name, age, location, previousBackground, className, favSubjects, grade ){
         super(name, age, location);
@@ -105,17 +76,8 @@ let newStudent1 = new Student('Sarah', 24, 'Dublin', 'MSc in Pharmaceuticals', '
 let newStudent2 = new Student('Tony', 29, 'Manila', 'BSc in Computing', 'WEBEU1', ['JS1','Responsive Design','Advanced CSS']);
 let newStudent3 = new Student('Peter', 38, 'Dublin', 'MSc in Pharmaceuticals', 'WEBE2', ['Data Science','UX/UI','iOS Development']);
 
-// #### Project Manager
 
-// * Now that we have instructors and students, we'd be nowhere without our PM's
-// * ProjectManagers are extensions of Instructors
-// * ProjectManagers have the following unique props:
-//   * `gradClassName`: i.e. CS1
-//   * `favInstructor`: i.e. Sean
-// * ProjectManagers have the following Methods:
-//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-
+//PM
 class ProjectManager extends Instructor {
     constructor(name, age, location, speciality, favLanguage, catchPhrase, gradClassName, favInstructor){
         super(name, age, location, speciality, favLanguage, catchPhrase);
@@ -133,11 +95,3 @@ class ProjectManager extends Instructor {
 let newPM1 = new ProjectManager('Alice', 31, 'Italy', 'CS', 'C+', 'Theres No I in Team', 'WEB20', 'Johnny');
 let newPM2 = new ProjectManager('Dylan', 26, 'Spain', 'Android Development', 'JS', 'My Cup of Tea', 'WEB22', 'Catherine');
 let newPM3 = new ProjectManager('Jamie', 42, 'Ireland', 'CS', 'HTML', 'Rain on Your Parade', 'WEB17', 'Stefan');
-
-// #### Stretch Problem
-
-// * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
-// * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
-// * Add a graduate method to a student.
-//   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
-//   * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,162 +1,162 @@
-// /* 
+/* 
 
-// Prototype Refactor
+Prototype Refactor
 
-// 1. Copy and paste your code or the solution from yesterday
+1. Copy and paste your code or the solution from yesterday
 
-// 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
-// */
+*/
 
-// /*
-// TASK 1
+/*
+TASK 1
 
-// - Build a Person Constructor that takes name and age.
-// - Give persons the ability to greet by returning a string stating name and age.
-// - Give persons the ability to eat edibles.
-// - When eating an edible, it should be pushed into a "stomach" property which is an array.
-// - Give persons the ability to poop.
-// - When pooping, the stomach should empty.
-// */
+- Build a Person Constructor that takes name and age.
+- Give persons the ability to greet by returning a string stating name and age.
+- Give persons the ability to eat edibles.
+- When eating an edible, it should be pushed into a "stomach" property which is an array.
+- Give persons the ability to poop.
+- When pooping, the stomach should empty.
+*/
 
-// class Person{
-//     constructor(name,age){
-//         this.name = name;
-//         this.age = age;
-//         this.stomach = [];
-//     }
-//     greet() {
-//         return `Hi my name is ${this.name} and my age is ${this.age}`;        
-//     }
-//     eat(edible) {
-//         if (edible) {
-//             this.stomach.push(edible);
-//           }
-//     }
-//     poop() {
-//         if (this.stomach.length > 0) this.stomach = [];
-//     }
-// }
-// let Meg = new Person('Megan', 21);
-// //TEST
-// // console.log(Meg);
-// // console.log(Meg.stomach);
-// // Meg.eat('pasta');
-// // console.log(Meg.stomach);
-// // Meg.eat('orange');
-// // console.log(Meg.stomach);
-// // Meg.poop();
-// // console.log(Meg.stomach);
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+        this.stomach = [];
+    }
+    greet() {
+        return `Hi my name is ${this.name} and my age is ${this.age}`;        
+    }
+    eat(edible) {
+        if (edible) {
+            this.stomach.push(edible);
+          }
+    }
+    poop() {
+        if (this.stomach.length > 0) this.stomach = [];
+    }
+}
+let Meg = new Person('Megan', 21);
+//TEST
+// console.log(Meg);
+// console.log(Meg.stomach);
+// Meg.eat('pasta');
+// console.log(Meg.stomach);
+// Meg.eat('orange');
+// console.log(Meg.stomach);
+// Meg.poop();
+// console.log(Meg.stomach);
 
-// /*
-// TASK 2
+/*
+TASK 2
 
-// - Build a Car constructor that takes model name and make.
-// - Give cars the ability to drive a distance.
-// - By driving a car, the distance driven should be added to an "odometer" property.
-// - Give cars the ability to crash.
-// - A crashed car can't be driven any more. Attempts return a string "I crashed at x miles!", x being the miles in the odometer.
-// - Give cars the ability to be repaired.
-// - A repaired car can be driven again.
+- Build a Car constructor that takes model name and make.
+- Give cars the ability to drive a distance.
+- By driving a car, the distance driven should be added to an "odometer" property.
+- Give cars the ability to crash.
+- A crashed car can't be driven any more. Attempts return a string "I crashed at x miles!", x being the miles in the odometer.
+- Give cars the ability to be repaired.
+- A repaired car can be driven again.
 
-// */
+*/
 
-// class Car{
-//     constructor(model, name, make){
-//         this.model= model;
-//         this.name= name;
-//         this.make = make;
-//         this.odometer = 0;
-//         this.canDrive = true;
-//     }
-//     drive(distance){
-//         if (!this.canDrive) {
-//              return `I crashed at ${this.odometer} miles!`;
-//         }
-//         this.odometer += distance;
-//     }
-//     crash() {
-//         this.canDrive = false;
-//     }
-//     repair() {
-//         this.canDrive = true;
-//     }
-// }
-// let newCar = new Car ('Toyota', 'Yarris', 'Blablabla');
+class Car{
+    constructor(model, name, make){
+        this.model= model;
+        this.name= name;
+        this.make = make;
+        this.odometer = 0;
+        this.canDrive = true;
+    }
+    drive(distance){
+        if (!this.canDrive) {
+             return `I crashed at ${this.odometer} miles!`;
+        }
+        this.odometer += distance;
+    }
+    crash() {
+        this.canDrive = false;
+    }
+    repair() {
+        this.canDrive = true;
+    }
+}
+let newCar = new Car ('Toyota', 'Yarris', 'Blablabla');
 
-// /* TEST
-// console.log(newCar);
-// newCar.drive(20);
-// console.log(newCar.odometer);// --> should return 20
-// newCar.crash();// --> should return I crashed at 20 miles!
-// newCar.repair();
-// newCar.drive(40);
-// newCar.odometer; // --> should return 60
-// */
+/* TEST
+console.log(newCar);
+newCar.drive(20);
+console.log(newCar.odometer);// --> should return 20
+newCar.crash();// --> should return I crashed at 20 miles!
+newCar.repair();
+newCar.drive(40);
+newCar.odometer; // --> should return 60
+*/
 
-// /*
-// TASK 3
+/*
+TASK 3
 
-// - Build a Baby constructor that subclasses the Person built earlier.
-// - Babies of course inherit the ability to greet, which can be strange.
-// - Babies should have the ability to play, which persons don't.
-// - By playing, a string is returned with some text of your choosing.
-// */
-// class Baby extends Person {
-//     playing() {  return `I am playing with my train`; }
-// } 
-// let newBaby = new Baby('Joe', 1);
-// //TEST
-// // console.log(newBaby) --> returns baby object with name Joe, age 1, playing method & inherits greet, eat and poop from Person class
+- Build a Baby constructor that subclasses the Person built earlier.
+- Babies of course inherit the ability to greet, which can be strange.
+- Babies should have the ability to play, which persons don't.
+- By playing, a string is returned with some text of your choosing.
+*/
+class Baby extends Person {
+    playing() {  return `I am playing with my train`; }
+} 
+let newBaby = new Baby('Joe', 1);
+//TEST
+// console.log(newBaby) --> returns baby object with name Joe, age 1, playing method & inherits greet, eat and poop from Person class
 
-// /*
-// TASK 4
+/*
+TASK 4
 
-// Use your imagination and come up with constructors that allow to build objects
-// With amazing and original capabilities. Build 3 small ones, or a very
-// complicated one with lots of state. Surprise us!
+Use your imagination and come up with constructors that allow to build objects
+With amazing and original capabilities. Build 3 small ones, or a very
+complicated one with lots of state. Surprise us!
 
-// */
+*/
 
-// class Library{
-//     constructor(libName, area, postCode){
-//         this.libName = libName;
-//         this.area = area;
-//         this.postCode = postCode;
-//         this.books = [];
-//     }
-//     addBook(book) {
-//         this.books.push(book);
-//     }
-//     checkoutBook(book){
-//         let isFound = this.books.includes(book);
-//         if(isFound) {
-//           book.isCheckedOut = true; 
-//           let bookIndex = this.books.indexOf(book);
-//           this.books.splice(bookIndex,1);
-//           return `You have checked out ${book.bookName}`;
-//         } else {
-//           return  `${book.bookName} isnt available right now :(`;
-//         }
-//     }
-// }
+class Library{
+    constructor(libName, area, postCode){
+        this.libName = libName;
+        this.area = area;
+        this.postCode = postCode;
+        this.books = [];
+    }
+    addBook(book) {
+        this.books.push(book);
+    }
+    checkoutBook(book){
+        let isFound = this.books.includes(book);
+        if(isFound) {
+          book.isCheckedOut = true; 
+          let bookIndex = this.books.indexOf(book);
+          this.books.splice(bookIndex,1);
+          return `You have checked out ${book.bookName}`;
+        } else {
+          return  `${book.bookName} isnt available right now :(`;
+        }
+    }
+}
 
-// class Book extends Library{
-//     constructor(libName, area, postCode, bookName, author, ISBN) {
-//         super( libName, area, postCode);
-//         this.bookName = bookName;
-//         this.author = author;
-//         this.ISBN = ISBN;
-//         this.isCheckedOut = false;
-//     }
-// }
+class Book extends Library{
+    constructor(libName, area, postCode, bookName, author, ISBN) {
+        super( libName, area, postCode);
+        this.bookName = bookName;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.isCheckedOut = false;
+    }
+}
 
-// //TEST
-// let megsLibrary = new Library('Megans Library', 'Malahide', 'K36 PK20');
-// let book1 = new Book('Megans Library', 'Malahide', 'K36 PK20', 'Eloquent JavaScript', 'Marijn Haverbeke', '1045435M');
+//TEST
+let megsLibrary = new Library('Megans Library', 'Malahide', 'K36 PK20');
+let book1 = new Book('Megans Library', 'Malahide', 'K36 PK20', 'Eloquent JavaScript', 'Marijn Haverbeke', '1045435M');
 
-// // megsLibrary; --> books array shouldnt hold anything
-// // megsLibrary.addBook(book1);
-// // megsLibrary; --> books array should hold book 1 now
-// // megsLibrary.checkoutBook(book1); --> should return you have checked out + book, book array should be empty again
-// // megsLibrary.checkoutBook(book1); --> should return book name + isnt available right now :(
+// megsLibrary; --> books array shouldnt hold anything
+// megsLibrary.addBook(book1);
+// megsLibrary; --> books array should hold book 1 now
+// megsLibrary.checkoutBook(book1); --> should return you have checked out + book, book array should be empty again
+// megsLibrary.checkoutBook(book1); --> should return book name + isnt available right now :(
